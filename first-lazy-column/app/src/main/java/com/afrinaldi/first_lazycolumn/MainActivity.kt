267620107f5b3c2db54958f39acdd2ef00d7da5d
AfrinaldiLibrary <afrinaldi190401@gmail.com>
2022-11-22 23:24:04 +0700

    implement first time lazt column

--- first-lazy-column/app/src/main/java/com/afrinaldi/first_lazycolumn/MainActivity.kt
+++ first-lazy-column/app/src/main/java/com/afrinaldi/first_lazycolumn/MainActivity.kt
@@ -1,16 +1,43 @@
 package com.afrinaldi.first_lazycolumn
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.outlined.ExpandMore
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.afrinaldi.first_lazycolumn.ui.theme.FirstLazyColumnTheme
+
+private val sampleName = listOf(
+    "udin",
+    "dadang",
+    "dudung",
+    "juned",
+    "kipli",
+    "suparman",
+    "sukija",
+    "popon",
+    "ujang"
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,26 +45,115 @@ class MainActivity : ComponentActivity() {
         setContent {
             FirstLazyColumnTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                FirstLazyColumnApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FirstLazyColumnApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background,
+    ) {
+        GreetingList(names = sampleName)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+
+    val animatedSizeDp by animateDpAsState(
+        targetValue = if (isExpanded) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow,
+        )
+    )
+
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.jetpack_compose),
+                contentDescription = "Logo",
+                modifier = Modifier.size(animatedSizeDp),
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column(
+                modifier = Modifier.weight(1f),
+            ) {
+                Text(
+                    text = "Hello $name!",
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Serif,
+                )
+                Text(
+                    text = "$name is what?",
+                    style = MaterialTheme.typography.body1.copy(
+                        fontStyle = FontStyle.Italic,
+                    )
+                )
+            }
+
+            IconButton(onClick = { isExpanded = !isExpanded }) {
+                Icon(
+                    imageVector = if (isExpanded) Icons.Filled.ExpandLess else Icons.Outlined.ExpandMore,
+                    contentDescription = if (isExpanded) "show less" else "show more"
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun DefaultPreview() {
     FirstLazyColumnTheme {
-        Greeting("Android")
+        Greeting(name = "Compose")
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL)
+@Preview(showBackground = true, device = Devices.PIXEL, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun FirstLazyColumnAppPreview() {
+    FirstLazyColumnTheme {
+        FirstLazyColumnApp()
+    }
+}
+
+
+@Composable
+fun GreetingList(names: List<String>) {
+    if (names.isNotEmpty()) {
+//        Column {
+//            for (name in names) {
+//                Greeting(name = name)
+//            }
+//        }
+
+        LazyColumn {
+            items(names) { name ->
+                Greeting(name)
+            }
+        }
+    } else {
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "No name")
+        }
     }
 }
